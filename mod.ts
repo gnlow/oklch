032c81d8fbf8f791ca8d5679ada7c51e919af7fc
Gnlow <gnlow@pm.me>
2026-09-13 10:43:38 +0900

    rgb

--- mod.ts
+++ mod.ts
@@ -4,3 +4,10 @@ export const hex =
 (l: number, c: number, h: number) =>
     new Color("oklch", [l, c, h])
         .toString({ format: "hex" })
+
+export const rgb =
+(l: number, c: number, h: number) =>
+    new Color("oklch", [l, c, h])
+        .to("srgb")
+        .coords
+        .map(v => Math.round(Math.max(0, Math.min(v, 1))*255))
